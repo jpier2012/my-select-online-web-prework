386c87da53bbd82d3b86d3dd5722052baeb6acc9
James Pier <jpier2012@gmail.com>
2019-04-04 01:20:06 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,10 @@ def my_select collection
   i = 0
   new_array = []
   while i < collection.size
-    new_array[i] = yield collection[i]
+    if yield collection[i]
+      new_array[i] = yield collection[i]
+    else
+    end
     i += 1
   end
   new_array
