f6e822ea988e7540d7d34251bb9f76e6d17dd4a6
James Pier <jpier2012@gmail.com>
2019-04-04 01:17:08 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,8 @@
-def my_select(collection)
- # your code here!
+def my_select collection
+  i = 0
+  new_array = []
+    while i < collection.size
+    new_array[i] = yield collection[i]
+    
+
 end
